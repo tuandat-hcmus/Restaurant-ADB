8e12d3714379e7eed6b300017a794647be1ad270
21120430 <ngotuandat.lop9a@gmail.com>
2024-12-25 22:18:19 +0700

    change theme pallete

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,10 +1,18 @@
-import MainLayout from "./layouts/MainLayout"
+import { createTheme, ThemeProvider } from '@mui/material'
+import MainLayout from './layouts/MainLayout'
+import { deepPurple, pink } from '@mui/material/colors'
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: pink,
+      secondary: deepPurple
+    }
+  })
   return (
-    <>
-    <MainLayout></MainLayout>
-    </>
+    <ThemeProvider theme={theme}>
+      <MainLayout></MainLayout>
+    </ThemeProvider>
   )
 }
 
